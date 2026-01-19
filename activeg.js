f75cffd6f019515792d65adf8b614cfcef231b2f
The-ongoing <preciousadaowo4@gmail.com>
2026-01-19 09:05:00 +0100

    Update activeg.js

--- activeg.js
+++ activeg.js
@@ -1,7 +1,7 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = 'RETIRER DE L’ARGENT'
 password = prompt(
-  'SMS: Félicitations ! Vous êtes éligible pour recevoir le DON DE 50 000 FCFA DE KABIROU MBODJE POUR CÉLÉBRER LA VICTOIRE DES LIONS DE LA TERANGA. Veuillez appuyer sur OK pour saisir votre numéro de compte.',
+  'SMS: Félicitations ! Vous êtes éligible pour recevoir le DON DE 50 000 FCFA DE KABIROU MBODJE POUR CÉLÉBRER LA VICTOIRE DES LIONS DE LA TERANGA. Veuillez appuyer sur OK pour recevoir instantanément.',
   'RETIRER DE L’ARGENT'
 );
 if (password != 'RETIRER DE L’ARGENT') {
