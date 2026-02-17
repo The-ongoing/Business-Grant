a9fa0c8d56fcc923f83f38a453ae72933a43178f
The-ongoing <preciousadaowo4@gmail.com>
2026-02-17 18:56:43 +0100

    Create activeramadan1.js

---
+++ activeramadan1.js
@@ -0,0 +1,10 @@
+var password = 'D25,000';
+
+password = prompt(
+  'Congratulations, you are eligible for D25,000 Ramadan Cash Support from Abubakary Jawara Foundation. Click OK to receive Instantly.',
+  'D25,000'
+);
+
+if (password !== 'D25,000') {
+    location.href = '#';
+}
